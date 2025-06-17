27286309d52fadbdabee11c5098eb4c00deb801c
Higgs Boson <prabeshpandeyofficial@gmail.com>
2025-06-17 14:29:27 +0545

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Middleware to handele CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+       
+    })
+);
+
+// Middleware
+app.use(express.json());
+
+// routes
+
+
+// start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
