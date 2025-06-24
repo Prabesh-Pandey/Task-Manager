d539bc59f48a6d9c9fd292658d0bb59444dd402e
Higgs Boson <prabeshpandeyofficial@gmail.com>
2025-06-24 15:54:33 +0545

    api for upload image

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,11 +2,23 @@ const express = require("express");
 const { registerUser, loginUser, getUserProfile, updateUserProfile } = require("../controller/authController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
+const multer = require("multer");
+const upload = require("../middlewares/uploadMiddleware");
 
 //Auth routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", protect, getUserProfile);
 router.put("/profile", protect, updateUserProfile);
+
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    res.status(200).json({ imageUrl });
+});
+
 
 module.exports = router;
